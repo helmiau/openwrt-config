77582e68307a712e368e9ddcec93bad024b8c0ae
Helmi Amirudin <helmilaw@gmail.com>
2021-03-10 07:04:09 +0700

    Update adguardhome-install.sh

--- adguardhome-install.sh
+++ adguardhome-install.sh
@@ -1,6 +1,7 @@
 mkdir /etc/AdGuardHome
 cd /etc/AdGuardHome/
-wget https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.105.1/AdGuardHome_linux_arm64.tar.gz -O AdGuardHome-bin.tar.gz
+wget https://static.adguard.com/adguardhome/release/AdGuardHome_linux_arm.tar.gz -O AdGuardHome-bin.tar.gz
+#wget https://github.com/AdguardTeam/AdGuardHome/releases/download/${latest_ver}/AdGuardHome_linux_${Arch}.tar.gz -O AdGuardHome-bin.tar.gz
 tar -xzvf AdGuardHome-bin.tar.gz
 cd AdGuardHome/
 ./AdGuardHome -s install
@@ -8,4 +9,3 @@ uci set dhcp.@dnsmasq[0].noresolv='1'
 uci add_list dhcp.@dnsmasq[0].server='127.0.0.1#53535'
 uci commit
 service dnsmasq reload
-
